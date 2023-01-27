dd0fa5c89d8dbc8fd11d03222f7b6899bfef6f9c
littlechaz <neon.fortytwo@gmail.com>
2023-01-27 12:24:19 +0000

    basic wip solution test with new code added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Main program
 """
 from config import get_config
-from thuwed_stalker import ThuwedStalker
+from thuwed_scraper import ThuwedScraper
 from dragcave_api import DragCaveAPI
 
 def main(cfg):
@@ -10,11 +10,35 @@ def main(cfg):
     program entry
     """
     dc_api = DragCaveAPI(cfg.get("APIKey"))
-    grabber = ThuwedStalker(dc_api)
-    grabber.find_pairs()
+    grabber = ThuwedScraper(dc_api)
 
-    # TODO automate script to grab codes, for testing purposes we use these.
-    #grabber.findPairs()
+    # For this to work, we need two sets of data
+    # 1 - A list of thuwed pairs
+    # 2 - A massview return of thuwed codes
+    pairs_as_codes = grabber.find_pairs()
+
+    # TODO for testing purposes, we're only going to fetch data for a set list
+    # of codes for now.
+    thuweds = dc_api.get_massview(set(["mdCF", "HAEc", "pf13", "9tfz"]))
+
+    def assign_dragon_data(code):
+        """
+        Here, we'll search our thuweds data for a matching code
+        and assign the data from it to our array
+        """
+        if thuweds.get(code):
+            return thuweds.get(code)
+        else:
+            return ''
+
+    # Loop through every pair and assign data from thuweds to each dragon
+    pairs = list(map(lambda pair: {
+        "m": assign_dragon_data(pair[0]),
+        "f": assign_dragon_data(pair[1])
+    }, pairs_as_codes))
+
+    print(pairs)
+
     # codes = ["ZTJsh"]
     # for code in codes:
     #     grabber.examineProgeny(code)
